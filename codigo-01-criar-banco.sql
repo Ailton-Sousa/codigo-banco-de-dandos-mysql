706ad80f3ff69f106685371a39513aadac599327
Ailton Sousa <ailton2017oliveira@gmail.com>
2023-08-12 00:41:55 -0300

    Update codigo-01-criar-banco.sql
    Trabalho com chave primarias

--- codigo-01-criar-banco.sql
+++ codigo-01-criar-banco.sql
@@ -9,12 +9,14 @@ DEFAULT COLLATE utf8_general_ci;
  * Selecionar banco
  */
 create table pessoas(
-nome varchar(30) not null,
-nasc date,
-sexo enum('M','F'),
-peso decimal(5,2),
-altura decimal(3,2),
-nacionalidade varchar(20) default'Brasil'
+    id int not null auto_increment,
+	nome varchar(30) not null,
+	nasc date,
+	sexo enum('M','F'),
+	peso decimal(5,2),
+	altura decimal(3,2),
+	nacionalidade varchar(20) default'Brasil',
+     primary key(id)
 ) default charset = utf8;
 
 use cadrato;
