5427d92a30edb02cd08bd53b3917fc38436ef074
Ailton Sousa <ailton2017oliveira@gmail.com>
2023-08-12 00:19:24 -0300

    Update codigo-01-criar-banco.sql
    Adicionando criação de tabelas

--- codigo-01-criar-banco.sql
+++ codigo-01-criar-banco.sql
@@ -4,3 +4,17 @@
 CREATE DATABASE cadrato
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
+/*
+ * Criando tabelas do banco de dados
+ * Selecionar banco
+ */
+create table pessoas(
+nome varchar(30) not null,
+nasc date,
+sexo enum('M','F'),
+peso decimal(5,2),
+altura decimal(3,2),
+nacionalidade varchar(20) default'Brasil'
+) default charset = utf8;
+
+use cadrato;
